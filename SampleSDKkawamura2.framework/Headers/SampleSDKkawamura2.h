392cf6ec8dd2e465e5546efbfe74693efbaefac2
gmoco-kawamura <158865565+gmoco-kawamura@users.noreply.github.com>
2024-02-27 18:29:58 +0900

    test

---
+++ SampleSDKkawamura2.framework/Headers/SampleSDKkawamura2.h
@@ -0,0 +1,18 @@
+//
+//  SampleSDKkawamura2.h
+//  SampleSDKkawamura2
+//
+//  Created by 川村拓也 on 2024/02/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleSDKkawamura2.
+FOUNDATION_EXPORT double SampleSDKkawamura2VersionNumber;
+
+//! Project version string for SampleSDKkawamura2.
+FOUNDATION_EXPORT const unsigned char SampleSDKkawamura2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleSDKkawamura2/PublicHeader.h>
+
+
